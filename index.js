5824286611a89e3da948c631d1275939cc8b27d5
Sorel-Haller <hallersorel@gmail.com>
2025-02-28 19:58:00 +0200

    About

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 /* Header dropdown */
-
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
+  function changeMainImage(thumbnail, labelText, labelTitle) {
+    document.getElementById('main-image').src = thumbnail.src;
+    
+    document.getElementById('label-text').textContent = labelText;
+    
+    document.getElementById('label-title').textContent = labelTitle;
+}
+
+function myFunction() {
+  let dropdown = document.querySelector(".dropdown-content");
+  // Toggle the dropdown visibility
+  dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+}
+
+
+/* Inspiration slides */
+
+let slideIndex = 1;
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("thumbnail");
+    let dots = document.getElementsByClassName("dot");
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex + 0].className += " active";
+}
+
+showSlides(slideIndex);
+
